5d1a7d02bc1ab9dc6528630531e97deb265d8ce4
Chase Johnson <charles-e-johnson@uiowa.edu>
2019-09-18 15:42:37 -0500

    Able to pull information from the database in a local host.

--- test_controller.js
+++ test_controller.js
@@ -1,20 +1,25 @@
 
 var mysql = require('mysql');
+var express = require('express')
+var app = express();
 
 var con = mysql.createConnection({
     host: "34.68.18.19",
     user: "root",
-    password: ""
-})
+    password: "dreamteam1"
+});
 
-sql = "select * from Lab1.TempData"
+sql = "select * from Lab1.TempData";
 
-tempArr = []
-timeArr = []
+tempArr = [];
+timeArr = [];
 
 con.connect(function(err){
     if (err) throw err;
     console.log("Connected");
+});
+
+app.get('/', function(req, resp){
     con.query(sql, function(err, result, fields){
         if (err) throw err;
         var i = 0;
@@ -23,7 +28,10 @@ con.connect(function(err){
             tempArr[i] = result[i].Temp;
             timeArr[i] = result[i].Time;
         }
+        resp.json(tempArr)
         console.log(tempArr);
         console.log(timeArr);
     });
 });
+
+app.listen(1337);
