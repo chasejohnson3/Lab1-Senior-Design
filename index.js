6526792ff12c2fb7ed4cf7a05e9329b35db6bbb6
Chase Johnson <charles-e-johnson@uiowa.edu>
2019-09-20 15:59:57 -0500

    Addedall the files from the heroku tutorial.

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-
+const express = require('express')
+const path = require('path')
 var mysql = require('mysql');
-var express = require('express');
 var app = express();
 const url = require('url');
-var fs = require("fs");
 
-var db_password = JSON.parse(fs.readFileSync("passwords.json")).password;
-
-
-
-
+const PORT = process.env.PORT || 5000
 
 var con = mysql.createConnection({
     host: "34.68.18.19",
     user: "root",
-    password: db_password
+    password: "dreamteam1"
 });
 
 sql = "select * from Lab1.TempData";
@@ -28,28 +23,35 @@ con.connect(function(err){
 });
 
 app.get('/', function(req, resp){
-    con.query(sql, function(err, result, fields){
-        if (err) throw err;
-        var i = 0;
-        for (i=0; i<result.length; i++)
-        {
-            tempArr[i] = result[i].Temp;
-            timeArr[i] = result[i].Time;
-        }
-        // resp.json(tempArr)
-        // resp.redirect("https://chasejohnson3.github.io/Lab1-Senior-Design/")
-        resp.redirect(url.format({
-            pathname:"https://chasejohnson3.github.io/Lab1-Senior-Design/",
-            query: {
-               "tempArr":JSON.stringify(tempArr),
-               "timeArr": JSON.stringify(timeArr)
+    // while(1){
+        con.query(sql, function(err, result, fields){
+            if (err) throw err;
+            var i = 0;
+            for (i=0; i<result.length; i++)
+            {
+                tempArr[i] = result[i].Temp;
+                timeArr[i] = result[i].Time;
             }
-        }));
-    
-        // resp.sendFile("C:\\Users\\User\\OneDrive - University of Iowa\\2019 Fall Semester\\Senior Design\\Lab1-Senior-Design\\htmlExample.html")
-        console.log(tempArr);
-        console.log(timeArr);
-    });
-});
-
-app.listen(1337);
+            // resp.redirect(url.format({
+            //     pathname:"https://chasejohnson3.github.io/Lab1-Senior-Design/",
+            //     query: {
+            //        "tempArr":JSON.stringify(tempArr),
+            //        "timeArr": JSON.stringify(timeArr)
+            //     }
+            // }));
+            // resp.sendFile("C:\\Users\\User\\OneDrive - University of Iowa\\2019 Fall Semester\\Senior Design\\node-js-getting-started\\index.html");
+            // resp.sendFile(__dirname + "/index.html");
+            resp.render(__dirname + "/index.ejs", {
+                tempArr: tempArr,
+                timeArr: timeArr
+            });
+            
+            
+            // console.log(tempArr);
+            // console.log(timeArr);
+        });
+        // setTimeout(30000);
+    // }
+  
+})
+app.listen(PORT);
