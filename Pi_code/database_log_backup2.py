25c2b15ebaf12befba7fb8c1e1cec924d842b80a
Chase Johnson <chaser.johnson27@gmail.com>
2019-10-03 14:44:50 -0500

    cleaning things up

--- Pi_code/database_log_backup2.py
+++ Pi_code/database_log_backup2.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
@@ -7,7 +8,7 @@ import os
 import glob
 import time
 import MySQLdb
-import urllib2
+
 DS18B20="/sys/bus/w1/devices/28-021316acc9aa/w1_slave"
 
 # Raspberry Pi pin configuration:
@@ -29,36 +30,23 @@ lcd_rows    = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                            lcd_columns, lcd_rows, lcd_backlight)
 
-wifiConnected = 0
-while (wifiConnected == 0):
-	try:
-		urllib2.urlopen("https://www.google.com/")
-	except urllib2.URLError, e:
-		lcd.message("wifi is poop")
-		time.sleep(5)
-	else:
-		lcd.message("WIFI READY")
-		wifiConnected = 1;
 
 #Set up the database connection
 db = MySQLdb.connect(host="34.68.18.19",
 			 user="root",
 			 passwd="dreamteam1",
 			 db="Lab1")
-	
 #Set up a cursor object so you can accomplish your desired queries
 cur = db.cursor()
 
 #Clear the database upon Pi boot
 cur.execute("truncate Lab1.TempData")
 db.commit()
-
+counter = 0;
 r=0
 while True:
 
-	r += 1
 	disPwr = False
-	
 	# If the sensor is not unplugged
 	if(os.path.isfile(DS18B20)):
 		f=open(DS18B20, "r")
@@ -72,25 +60,26 @@ while True:
 		(discard, sep, reading)=data.partition(" t=")
 		tc = float(reading)/1000.0
 		tf = tc*9.0/5.0 + 32.0
-
+                
 		#Put any SQL command here - In our case, put sensor data in database
-		cur.execute("INSERT INTO TempData (idTempData, Temp, Time) VALUES(%s, %s, %s)",(r, tc, r))
+                r +=1
+                cur.execute("INSERT INTO TempData (idTempData, Temp, Time) VALUES(%s, %s, %s)",(r, tc, r))
 
-		db.commit()
+                db.commit()
 
-		cur.execute("select * from Lab1.DisplayStatus")
-		resultSet = cur.fetchall()
-		for row in resultSet:
-			print row[0], row[1]
-		db.commit()
-
-
-		if(row[1]):
-			lcd.clear()
-			lcd.message("{} {:.2f}C {:.2f}F Working = {}".format(r,tc,tf,yesorno))
-			print("{} {:.2f}C {:.2f}F Working = {}".format(r,tc,tf,yesorno))
-		else:
-			lcd.clear()
+                cur.execute("select * from Lab1.DisplayStatus")
+                resultSet = cur.fetchall()
+                for row in resultSet:
+                    print row[0], row[1]
+                db.commit()
+                
+            
+                if(row[1]):
+                        #ld.clear()
+                        #lcd.message("{} {:.2f}C {:.2f}F Working = {}".format(r,tc,tf,yesorno))
+                        print("{} {:.2f}C {:.2f}F Working = {}".format(r,tc,tf,yesorno))
+                else:
+                        lcd.clear()
 	else:
 		cur.execute("INSERT INTO TempData (idTempData, Temp, Time) VALUES(%s, %s, %s)",(r, None, r))
 		db.commit()
@@ -98,4 +87,3 @@ while True:
 		lcd.message("Error reading data")
 		
 	#Delay 1s between readings
-	time.sleep(0.5)
